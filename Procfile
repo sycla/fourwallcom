158397e13d02d46fc07cf78022b590f92865ac24
sycla <91137677+sycla@users.noreply.github.com>
2023-02-06 15:59:11 +0500

    first upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fourwalls.wsgi:application --log-file -
---
+++ fourwalls/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for fourwalls project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fourwalls.settings')
+
+application = get_asgi_application()
